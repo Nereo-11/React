63614e61ca6f272fddb66427455b8bc5b7ae0ad2
NereoUriel <115206702+Nereo-11@users.noreply.github.com>
2024-10-18 23:05:43 -0600

    Primeras Funciones de Repositorio

--- src/Paginas/repository.tsx
+++ src/Paginas/repository.tsx
@@ -1,9 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.tsx";
 import './repository.css';
+import { useState } from "react";
+
 
 function Repository() {
     const navigate = useNavigate();
+    const [data, setData] = useState([]);
+
+    async function searchKey (key : string) {
+        const response = await fetch(`http://127.0.0.1:8000/search-api/?key=${key}`); // Cambia esta URL por la de tu API
+        const result = await response.json();
+        const a = JSON.parse(result)
+        setData(a);
+    }
+
 
     return(
         <>
@@ -15,6 +26,7 @@ function Repository() {
             <h1>Plataforma Genómica</h1>
         </div>
 
+        <body>
         <section className={'section'}>
         <div className={'search'}>
 
@@ -25,14 +37,14 @@ function Repository() {
 
         <section className={'section-left'}>
         <div className={'target-l'}>
-        <button>
+        <button onClick={() => searchKey('human')}>
         <h4>Humanos</h4>
         </button>
         </div>
         <div className={'target-l'}>
-        <button>
+        <button onClick={() => searchKey('animal')}>
         <h4>Animales</h4>
-        </button>
+        </button >
         </div>
         <div className={'target-l'}>
         <button>
@@ -46,6 +58,28 @@ function Repository() {
         
         </div>
         </section>
+
+        <div>
+        <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Organismo</th>
+            <th>Download</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item: { nombre: string; especie: string; descarga: string }, index) => (
+            <tr key={index}>
+              <td>{item.nombre}</td>
+              <td>{item.especie}</td>
+              <td>{item.descarga}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+        </div>
+        </body>
 
         <div>
             <p>Repository</p>
