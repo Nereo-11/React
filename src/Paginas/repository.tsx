3856082b41885ec7c4640496abade0c730f1c992
TheLeck <survigam@gmail.com>
2024-11-01 15:36:44 -0600

    funcionalidad del repositorio

--- src/Paginas/repository.tsx
+++ src/Paginas/repository.tsx
@@ -1,19 +1,39 @@
-import { json, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.tsx";
 import './repository.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function Repository() {
-    const navigate = useNavigate();
     const [data, setData] = useState([]);
+    const [keyword, setKeyword] = useState('');
+
 
     async function searchKey (key : string) {
         const response = await fetch(`http://127.0.0.1:8000/search-api/?key=${key}`); // Cambia esta URL por la de tu API
         const result = await response.json();
         const a = JSON.parse(result)
         setData(a);
+        console.log("paso por aqui")
     }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setKeyword(event.target.value); // Actualiza el estado con el nuevo valor del input
+    };
+
+    const search = ()=> {
+      setData([]);
+      searchKey(keyword)
+    };
+
+    const categories = (a: string) => {
+      setData([]);
+      setKeyword('');
+      searchKey(a)
+    }
+
+    useEffect(() => {
+        searchKey('human');       
+    }, []);
 
 
     return(
@@ -22,67 +42,53 @@ function Repository() {
             <Navbar/>
         </header>
 
-        <div className={'title'}>
-            <h1>Plataforma Genómica</h1>
-        </div>
-
-        <div className={"wrapper"}>
-        <div className={'search'}>
-
-            <input id = "busqueda" type="text" className={'public'}></input>
-
-        </div>
-        </div>
-        <body>
-
+        {data.length != 0 ?
         
-
-        <div className={'div-left'}>
-        <div className={'target-l'}>
-        <button onClick={() => searchKey('human')}>
-        <h4>Humanos</h4>
-        </button>
-        </div>
-        <div className={'target-l'}>
-        <button onClick={() => searchKey('animal')}>
-        <h4>Animales</h4>
-        </button >
-        </div>
-        <div className={'target-l'}>
-        <button onClick={() => searchKey('planta')} >
-        <h4>Plantas</h4>
-        </button>
-        </div>
-        </div>
-
-
-        <div className={'table'}>
-        <table>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Organismo</th>
-            <th>Download</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item: { nombre: string; especie: string; descarga: string }, index) => (
-            <tr key={index}>
-              <td>{item.nombre}</td>
-              <td>{item.especie}</td>
-              <td> <a href={item.descarga} target="_blank" rel="noopener noreferrer">Download</a></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-        </div>
-        </body>
-
         <div>
-            <p>Repository</p>
-            <button onClick={() => navigate('/home')}>Home</button>
-            <button onClick={() => navigate('/')}>App</button>
+
+          <div className={'div-left'}>
+            <button onClick={() => categories('human')} className={'target-l'}>
+              <h4>Humanos</h4>
+            </button>
+            <button onClick={() => categories('animal')} className={'target-l'}>
+              <h4>Animales</h4>
+            </button >
+            <button onClick={() => categories('planta')} className={'target-l'}>
+              <h4>Plantas</h4>
+            </button>
+          </div>
+
+          <div className={"wrapper"}>
+          <label htmlFor="busqueda">Busqueda: </label>
+          <div className={'search'}>
+              <input id = "busqueda" type="text" className={'public'} value={keyword} onChange={handleInputChange}></input>
+          </div>
+            <button onClick={search}>Buscar</button>
+          </div>
+
+          <div className={'table'}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Organismo</th>
+                  <th>Download</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item: { nombre: string; especie: string; descarga: string }, index) => (
+                  <tr key={index}>
+                    <td>{item.nombre}</td>
+                    <td>{item.especie}</td>
+                    <td> <a href={item.descarga} target="_blank" rel="noopener noreferrer">Download</a></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+        
+        : <p>Cargando resultados... por favor espere. </p>}
 
         </>
     )
